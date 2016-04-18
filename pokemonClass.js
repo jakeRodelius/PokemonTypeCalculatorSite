2096a8450121d1d0b57d164948e62b3f0872bcc0
Jake <jakebrodelius@gmail.com>
2016-04-18 15:50:10 -0500

    made it functional

--- pokemonClass.js
+++ pokemonClass.js
@@ -1,4 +1,4 @@
-var typeNames = ['normal', 'fire', 'water', 'electric', 'grass', 'ice', 'fighting', 'poison', 'ground', 'flying', 'psychic', 'bug', 'rock', 'ghost', 'dragon', 'dark', 'steel', 'fairy']
+var typeNames = ['Normal', 'Fire', 'Water', 'Electric', 'Grass', 'Ice', 'Fighting', 'Poison', 'Ground', 'Flying', 'Psychic', 'Bug', 'Rock', 'Ghost', 'Dragon', 'Dark', 'Steel', 'Fairy']
 var chart = [ 
     [001, 001, 001, 001, 001, 001, 001, 001, 001, 001, 001, 001, 001, 001, 001, 001, 001, 001], //None
     [001, 001, 001, 001, 001, 001, 002, 001, 001, 001, 001, 001, 001, 000, 001, 001, 001, 001], //Normal
@@ -21,32 +21,75 @@ var chart = [
     [001, 001, 001, 001, 001, 001, 0.5, 002, 001, 001, 001, 0.5, 001, 001, 000, 0.5, 002, 001] //Fairy
 ];
 
-class Pokemon {
-	constructor(type1 =0, type2 =0){
-		this.type1 = type1;
-		this.type2 = type2;
+function Pokemon(arg){
+	var number = arg;
+	var type1 = 0;
+	var type2 = 0;
+
+	this.changeType = function(slot, type){
+		if(slot == 1)
+			type1 = type;
+		if(slot == 2)
+			type2 = type;
 	}
 
-    type(slot=1, newType){
-        if(slot==1){
-            type1 = newType;
-        } 
-        else {
-            type2 = newType;
+	this.calcWeakness = function(){
+        var result = [];
+        var index = 0;
+        for(i = 0; i<18; i++){
+            if(chart[type1][i] * chart[type2][i] >= 2){
+                result[index] = typeNames[i];
+                index++;
+            }
         }
-    }
+        document.getElementById("weakness" + number).innerHTML = "Weaknesses: " + arrayToString(result, index);
+        return arrayToString(result, index);
+	}
 
-    get typeInSlot(slot){
-        if(slot == 1)
-            return type1;
-        else
-            return type2;
-    }
-
-    get typeName (slot) {
-        return typeNames[typeInSlot(slot)];
-    }
+	this.calcResist = function(){
+		var result = [];
+		var index = 0;
+        for(i = 0; i<18; i++){
+            if(chart[type1][i] * chart[type2][i] <= 0.5){
+                result[index] = typeNames[i];
+                index++;
+            }
+        }
+        document.getElementById("resistance" + number).innerHTML = "Resistances: " + arrayToString(result, index);
+		return arrayToString(result, index);
+	}
 }
 
-var myPokemon = new Pokemon(1, 2);
-console.log("The pokemon is " myPokemon.typeName(1) + " and " + myPokemon.typeName(2));
+function calcTeamWeakness(){
+	myPokemon.calcWeakness();
+}
+
+function calcTeamResistance(){
+	myPokemon.calcResist();
+}
+
+function calculateTeam(){
+	myPokemon = new Pokemon(1); //this format makes it global automatically, javascript is weird
+	myPokemon.changeType(1, document.getElementById("pokemon1type1").options[document.getElementById("pokemon1type1").selectedIndex].value);
+	myPokemon.changeType(2, document.getElementById("pokemon1type2").options[document.getElementById("pokemon1type2").selectedIndex].value);
+	calcTeamWeakness();
+	calcTeamResistance();
+	// makeOptions();
+}
+
+function arrayToString(incoming, length){
+	var output = "";
+	for(i=0; i<length; i++){
+		output+=incoming[i];
+		if(i<length-1)
+			output+=", ";
+	}
+	return output;
+}
+
+function makeOptions(){
+	list = "<option value=\"0\">None</option>\n";
+	for(i = 0; i<18; i++)
+		list+="<option value=\""+ (i+1) + "\">"+ typeNames[i] +"</option>\n";
+	console.log(list);
+}
